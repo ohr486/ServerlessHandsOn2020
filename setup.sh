f9f94a6b59fa266d687d55dd765e25373a2b019b
ohr486 <ohr486@gmail.com>
2020-08-14 18:31:56 +0900

    update setup.sh, check tool and version

--- setup.sh
+++ setup.sh
@@ -2,5 +2,18 @@
 
 echo "=== SETUP serverless handson 2020 Envs ==="
 
-echo "checkout repo"
+echo "-----> check dev tools"
+for t in git curl npm node; do
+  echo "$t: `which $t`"
+done
+echo
+
+echo "-----> checkout repo"
+rm -rf workdir
 git clone git@github.com:ohr486/ServerlessHandsOn2020.git workdir
+echo
+
+echo "-----> check versions"
+echo "npm: `npm --version`"
+echo "node: `node --version`"
+echo
