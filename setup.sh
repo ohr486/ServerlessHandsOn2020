15a8710212ee8a549bf1d6a4f25fedf4a8c3073a
ohr486 <ohr486@gmail.com>
2020-08-14 18:13:36 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "this is setup script."
+echo "=== SETUP serverless handson 2020 Envs ==="
+
+mkdir workdir
+cd workdir
+git clone git@github.com:ohr486/ServerlessHandsOn2020.git
