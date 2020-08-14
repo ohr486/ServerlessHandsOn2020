0d56b9cd3e21bf20f83944c3853bd8c277dcbf2d
ohr486 <ohr486@gmail.com>
2020-08-14 18:17:47 +0900

    udpate rm

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ServerlessHandsOn2020
+
+## setup env
+
+```bash
+$ curl -s https://raw.githubusercontent.com/ohr486/ServerlessHandsOn2020/master/setup.sh | bash
+```
--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 
 echo "=== SETUP serverless handson 2020 Envs ==="
 
-mkdir workdir
-cd workdir
-git clone git@github.com:ohr486/ServerlessHandsOn2020.git
+echo "checkout repo"
+git clone git@github.com:ohr486/ServerlessHandsOn2020.git workdir
