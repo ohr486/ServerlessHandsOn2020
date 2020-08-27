7fd80b4e2787fad9bdc1fb29cb7b139bfb64f0ef
ohr486 <ohr486@gmail.com>
2020-08-27 12:23:04 +0900

    add show-cmd

--- show-cmd.sh
+++ show-cmd.sh
@@ -8,7 +8,7 @@ echo サービス名: $SERVICE
 echo サービスのソースのディレクトリ: $WORKDIR
 echo
 
-echo --- step.1 serverlessframewrokをインストール ---
+echo --- step.5 serverlessframewrokをインストール ---
 echo インストール実行コマンド:
 echo npm install -g serverless
 echo
@@ -16,22 +16,19 @@ echo バージョンチェックコマンド:
 echo serverless --version
 echo
 
-echo --- step.2 serverlessで雛形を作成 ---
+echo --- step.6.1 serverlessで雛形を作成 ---
 echo サービス作成コマンド:
-echo serverless create --template aws-nodejs --name $SERVICE --path $SERVICE
+echo sls create --template aws-nodejs --name $SERVICE --path $SERVICE
 echo
 
-echo --- step.3 ハンドラ/serverless.yml修正 ---
-echo
-
-echo --- step.4 デプロイメント/サーバー反映 ---
+echo --- step.6.5/7.4/8.3 デプロイメント/サーバー反映 ---
 echo sls deploy
 echo
 
-echo --- step.5 関数の実行 ---
+echo --- step.6.6/7.5 関数の実行 ---
 echo sls invoke --function \<関数名\>
 echo
 
-echo --- step.x 関数の削除 ---
+echo --- step.10 関数の削除 ---
 echo sls remove
 echo
